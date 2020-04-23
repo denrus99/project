1de33fc1a8451ed667befbf883837d1e6491129f
denrus99 <denrus99@gmail.com>
2020-04-23 14:25:01 +0500

    Final step of assemble

--- Crocodile/ClientApp/src/App.js
+++ Crocodile/ClientApp/src/App.js
@@ -11,44 +11,22 @@ import {GameComponent as Game} from "./Components/GameComponent";
 import {GameMasterComponent as GameMaster} from "./Components/GameMasterComponent";
 
 function App() {
-    return (
-        <div className="App">
-            <HeaderComponent/>
-            <div className='container' style={{width: '98%', marginTop: '8em' }}>
-                <Router>
-                    <Switch>
-                        <Route exact path='/' component = {Main}/>
-                        <Route exact path='/Profile' component = {Profile}/>
-                        <Route exact path='/Game' component = {Game}/>
-                        <Route exact path='/GameMaster' component = {GameMaster}/>
-                    </Switch>
-                </Router>
-            </div>
+  return (
+    <div className="App">
+      <HeaderComponent/>
+      <div className='container' style={{width: '98%', marginTop: '8em' }}>
+          <Router>
+              <Switch>
+                  <Route exact path='/' component = {Main}/>
+                  <Route exact path='/Profile' component = {Profile}/>
+                  <Route exact path='/Game' component = {Game}/>
+                  <Route exact path='/GameMaster' component = {GameMaster}/>
+              </Switch>
+          </Router>
+      </div>
 
-        </div>
-    );
+    </div>
+  );
 }
 
 export default App;
-// import React, { Component } from 'react';
-// import { Route } from 'react-router';
-// import { Layout } from './components/Layout';
-// import { Home } from './components/Home';
-// import { FetchData } from './components/FetchData';
-// import { Counter } from './components/Counter';
-//
-// import './custom.css'
-//
-// export default class App extends Component {
-//   static displayName = App.name;
-//
-//   render () {
-//     return (
-//       <Layout>
-//         <Route exact path='/' component={Home} />
-//         <Route path='/counter' component={Counter} />
-//         <Route path='/fetch-data' component={FetchData} />
-//       </Layout>
-//     );
-//   }
-// }
--- Crocodile/ClientApp/src/Input.js
+++ Crocodile/ClientApp/src/Input.js
@@ -1,4 +1,4 @@
-﻿import React, {Component} from 'react';
+import React, {Component} from 'react';
 
 import facebookLogo from './images/registrationForm/facebook_Icon.svg';
 import vkLogo from './images/registrationForm/vk_Icon.svg';
@@ -46,36 +46,36 @@ export class Input extends Component {
                         </td>
                     </tr>
                 </table>
-                <a  type="submit" onClick={(parent)=>this.props.signIn()} className="myButtonIn">Войти</a>
+                <a  type="submit" onClick={(parent)=>this.props.signIn()} className="button myButtonIn">Войти</a>
                 <span style={{position: 'absolute', fontSize: '2em', left: '15em', top: '32em'}}>
         Нет аккаунта? <a onClick={(x) => {this.setState({isInput: false})}} style={{cursor:"pointer",display: 'inline-block'}}><h4
                     style={{textDecoration: 'underline'}}>Регистрация</h4></a>
     </span>
             </form>;
         else return <form id="formUp">
-            <div style={{position: 'relative'}}>
-                <a onClick={(x) => this.setState({isInput: true})}><img style={{cursor:"pointer",
-                    height: '6em', width: '6em',
-                    position: 'absolute', top: '3em', left: '0.5em'
-                }} src={back}/></a>
-                <h2 style={{
-                    top: '.5em', left: '1.9em', position: 'absolute', fontSize: '6em',
-                    margin: '0 auto', textDecoration: 'underline'
-                }}>Регистрация</h2>
-                <input className="input" style={{top: '5em', left: '3em'}} id="registrationForm" type="text"
-                       defaultValue=""
-                       placeholder="Логин"
-                       required/>
-                <input className="input" style={{top: '9em', left: '3em'}} id="password" type="password"
-                       defaultValue=""
-                       placeholder="Пароль"
-                       required/>
-                <input className="input" style={{top: '12em', left: '3em'}} id="confirmPassword" type="password"
-                       defaultValue=""
-                       placeholder="Подтвердите пароль"
-                       required/>
-                <a type="submit" onClick={()=>this.props.signIn()} className="myButtonUp">Подтвердить</a>
-            </div>
-        </form>;
+                <div style={{position: 'relative'}}>
+                    <a onClick={(x) => this.setState({isInput: true})}><img style={{cursor:"pointer",
+                        height: '6em', width: '6em',
+                        position: 'absolute', top: '3em', left: '0.5em'
+                    }} src={back}/></a>
+                    <h2 style={{
+                        top: '.5em', left: '1.9em', position: 'absolute', fontSize: '6em',
+                        margin: '0 auto', textDecoration: 'underline'
+                    }}>Регистрация</h2>
+                    <input className="input" style={{top: '5em', left: '3em'}} id="registrationForm" type="text"
+                           defaultValue=""
+                           placeholder="Логин"
+                           required/>
+                    <input className="input" style={{top: '9em', left: '3em'}} id="password" type="password"
+                           defaultValue=""
+                           placeholder="Пароль"
+                           required/>
+                    <input className="input" style={{top: '12em', left: '3em'}} id="confirmPassword" type="password"
+                           defaultValue=""
+                           placeholder="Подтвердите пароль"
+                           required/>
+                    <a type="submit" onClick={()=>this.props.signIn()} className="button myButtonUp">Подтвердить</a>
+                </div>
+            </form>;
     }
 }
