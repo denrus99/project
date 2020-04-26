c037cc1d09531c407ff3621ec7697088903d7f39
MrSasha <aa.kashin@mail.ru>
2020-04-26 17:42:26 +0500

    Edit repository

--- Crocodile/Startup.cs
+++ Crocodile/Startup.cs
@@ -1,3 +1,4 @@
+using DBProject;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -21,7 +22,7 @@ namespace Crocodile
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-
+            //services.AddSingleton<IUserRepository, MongoUserRepository>();
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
         }
