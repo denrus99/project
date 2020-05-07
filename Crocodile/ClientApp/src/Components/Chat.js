6f808aece3e4eab483cb626f105f23ba327e49f8
DmitriySoch <demon63176@gmail.com>
2020-05-07 15:03:10 +0500

    Add paint tools

--- Crocodile/ClientApp/src/Components/Chat.js
+++ Crocodile/ClientApp/src/Components/Chat.js
@@ -30,19 +30,24 @@ export class Chat extends Component {
 
     sendMessage(text, user) {
         let date = new Date(Date.now());
+        let block = document.getElementById("chatBlock");
         let msg = {
             user: {name: user.name, photo: photo},
             text: text,
             date: `${date.getHours()}:${date.getMinutes()}`
         }
         messages.push(msg);
-        this.setState({messages:messages});
+        this.setState({messages: messages});
+            let timer = setTimeout(() => {
+                block.scrollTop = block.scrollHeight;
+            }, 10)
+
     }
 
     render() {
         return (
             <div>
-                <div className='chat_Container'>
+                <div id='chatBlock' className='chat_Container'>
                     {messages.map(x => <Message user={x.user} text={x.text} date={x.date}/>)}
                 </div>
                 <Input sendMsg={this.sendMessage}/>
@@ -60,12 +65,13 @@ class Message extends Component {
         return (
             <div className='Message'>
                 <Popup
-                trigger={ <a href='/Profile'><img style={{minWidth: '40px', minHeight: '40px', maxHeight: '40px', maxWidth: '40px'}}
-                                  src={this.props.user.photo}/></a>}
-                position='top center' on='hover'>
-                    <h1>{this.props.user.name}</h1>
+                    trigger={<a href='/Profile' style={{maxHeight: '40px'}}><img
+                        style={{minWidth: '40px', minHeight: '40px', maxHeight: '40px', maxWidth: '40px'}}
+                        src={this.props.user.photo}/></a>}
+                    position='top center' contentStyle={{zIndex: 11, width: 'inherit'}} on='hover'>
+                    <h1 style={{padding: '0 20px'}}>{this.props.user.name}</h1>
                 </Popup>
-               
+
                 <div className='MessageContainer'>
                     <h2>{this.props.text}</h2>
                     <h3>{this.props.date}</h3>
@@ -81,13 +87,15 @@ class Input extends Component {
         this.inputRef = React.createRef();
         this.Send = this.Send.bind(this);
     }
-    Send(){
+
+    Send() {
         let input = this.inputRef.current;
-        if(input.value!== ''){
-            this.props.sendMsg(input.value,{name: 'Вера', photo: photo});
+        if (input.value !== '') {
+            this.props.sendMsg(input.value, {name: 'Вера', photo: photo});
             input.value = '';
         }
     }
+
     render() {
         return (
             <div className='Input_Container'>
