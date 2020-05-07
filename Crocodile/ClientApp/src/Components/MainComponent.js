eb156fc919211e86fa240f420600d16ca67f27c3
DmitriySoch <demon63176@gmail.com>
2020-05-07 19:25:05 +0500

    Add page navigation on main page

--- Crocodile/ClientApp/src/Components/MainComponent.js
+++ Crocodile/ClientApp/src/Components/MainComponent.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import './mainpagestyle.css';
 import Popup from "reactjs-popup";
-import {Input} from "../Input";
+import backArrow from '../images/Main/arrowsBack.svg';
+import nextArrow from '../images/Main/arrowNext.svg';
+
 
 export class MainComponent extends Component {
     render() {
@@ -15,9 +17,9 @@ export class MainComponent extends Component {
     }
 }
 
-class ServerInfo extends Component{
+class ServerInfo extends Component {
     render() {
-        return(
+        return (
             <div className='serverInfo'>
                 <h1>Статус сервера</h1>
                 <table>
@@ -68,11 +70,11 @@ class Sidebar extends Component {
                                     </td>
                                 </tr>
                             </table>
-                            
+
                         </>
                     )}
                 </Popup>
-                <Popup modal trigger={<h1 className='fontStyle'>Присоединиться</h1>}> 
+                <Popup modal trigger={<h1 className='fontStyle'>Присоединиться</h1>}>
                     {close => (
                         <>
                             <a className="close" onClick={close}>
@@ -102,16 +104,43 @@ class Sidebar extends Component {
 }
 
 class OpenGames extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {pageNum: 0};
+        this.changePage = this.changePage.bind(this);
+    }
+
+    changePage(x) {
+        debugger
+        this.setState({pageNum: this.state.pageNum + x});
+
+    }
+
     render() {
         //fetch("Запрос открытых игр");
         let openGames = [];
-        for (let i = 1; i < 51; i++) {
-            openGames.push({name: `Лобби ${i}`, info: 'Какая-то информация', id: i});
+        for (let i = 0; i < 100; i++) {
+            openGames.push({name: `Лобби ${i + 1}`, info: 'Какая-то информация', id: i + 1});
         }
+
         return (
             <div className='OpenGamesContainer'>
                 <h1>Открытые игры</h1>
-                {openGames.map(x => <LobbyItem name={x.name} info={x.info}/>)}
+                {openGames.slice(this.state.pageNum * 10, (this.state.pageNum + 1) * 10 - 1).map(x => <LobbyItem
+                    name={x.name} info={x.info}/>)}
+                <div style={{margin: '0 auto', justifyContent:'space-around', alignItems:'center'}} className='rowContainer'>
+                    {
+                        this.state.pageNum > 0 ?
+                            <img className='arrowImg' src={backArrow} onClick={() => this.changePage(-1)}/>
+                            :  <div className='arrowImg'/>
+                    }
+                    <h1>{this.state.pageNum}</h1>
+                    {
+                        this.state.pageNum < (openGames.length / 10) - 1 ?
+                            <img  className='arrowImg' src={nextArrow} onClick={() => this.changePage(1)}/>:
+                            <div className='arrowImg'/>
+                    }
+                </div>
             </div>
         );
     }
@@ -123,7 +152,7 @@ class LobbyItem extends Component {
             <div className='lobbyItem'>
                 <h2>{this.props.name}</h2>
                 <h3>{this.props.info}</h3>
-                <button>Войти</button>
+                <button onClick={()=>window.location.href = '/game1'}>Войти</button>
             </div>
         );
     }
