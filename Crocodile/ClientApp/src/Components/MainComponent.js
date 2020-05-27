172153c84edaeea37f50c972da459565366b5e45
DmitriySoch <demon63176@gmail.com>
2020-05-27 16:21:53 +0500

    Add loading animation

--- Crocodile/ClientApp/src/Components/MainComponent.js
+++ Crocodile/ClientApp/src/Components/MainComponent.js
@@ -3,34 +3,18 @@ import './mainpagestyle.css';
 import backArrow from '../images/Main/arrowsBack.svg';
 import nextArrow from '../images/Main/arrowNext.svg';
 import {Link} from "react-router-dom"
+import {Roller} from "react-spinners-css";
 
 
 export class MainComponent extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state={isLoad:false}        
+    }
+    render() {        
         return (
             <div className='rowContainer'>
-                <OpenGames/>
-                <ServerInfo/>
-            </div>
-        );
-    }
-}
-
-class ServerInfo extends Component {
-    render() {
-        return (
-            <div className='serverInfo'>
-                <h1>Статус сервера</h1>
-                <table>
-                    <tr>
-                        <td><h1>Количество пользователей онлайн</h1></td>
-                        <td><h2>1</h2></td>
-                    </tr>
-                    <tr>
-                        <td><h1>Количество игр</h1></td>
-                        <td><h2>0</h2></td>
-                    </tr>
-                </table>
+                {this.props.isLoad?<OpenGames openGames={new []}/>:<div className="middleContainer"><Roller color="black"/></div>}
             </div>
         );
     }
@@ -49,16 +33,11 @@ class OpenGames extends Component {
     }
 
     render() {
-        //fetch("Запрос открытых игр");
-        let openGames = [];
-        for (let i = 0; i < 100; i++) {
-            openGames.push({name: `Лобби ${i + 1}`, info: 'Какая-то информация', id: i + 1});
-        }
 
         return (
             <div className='OpenGamesContainer'>
                 <h1>Открытые игры</h1>
-                {openGames.slice(this.state.pageNum * 10, (this.state.pageNum + 1) * 10 - 1).map(x => <LobbyItem
+                {this.props.openGames.slice(this.state.pageNum * 10, (this.state.pageNum + 1) * 10 - 1).map(x => <LobbyItem
                     name={x.name} info={x.info}/>)}
                 <div style={{margin: '0 auto', justifyContent:'space-around', alignItems:'center'}} className='rowContainer'>
                     {
@@ -68,7 +47,7 @@ class OpenGames extends Component {
                     }
                     <h1>{this.state.pageNum}</h1>
                     {
-                        this.state.pageNum < (openGames.length / 10) - 1 ?
+                        this.state.pageNum < (this.props.openGames.length / 10) - 1 ?
                             <img  className='arrowImg' src={nextArrow} onClick={() => this.changePage(1)}/>:
                             <div className='arrowImg'/>
                     }
